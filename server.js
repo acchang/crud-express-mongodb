6648a13c35c9ee5f0e4e433a2837697e7b6dbd22
Andrew Chang <acchang4@gmail.com>
2022-06-01 15:27:18 -0400

    clened up after dotenv add

--- server.js
+++ server.js
@@ -2,14 +2,10 @@ const express = require('express');
 const bodyParser= require('body-parser');
 const app = express();
 const MongoClient = require('mongodb').MongoClient
-// const connectionString = 'mongodb+srv://Bloop:bleep@cluster0.jqbcy.mongodb.net/?retryWrites=true&w=majority'
 
-// from https://discord.com/channels/735923219315425401/980280502244212786/980298549889736744
 const dotenv = require('dotenv') // .env file
 dotenv.config() // using .env
 const connectionString = `mongodb+srv://${process.env.USERNAME}:${process.env.PW}@cluster0.jqbcy.mongodb.net/?retryWrites=true&w=majority`;
-
-console.log(`${process.env.USERNAME}`, `${process.env.PW}`);
 
 // All your handlers here...
 
@@ -21,7 +17,6 @@ MongoClient.connect(connectionString, {
     if (err) return console.error(err)
     console.log('Connected to Database')
   })
-
 
 // MongoClient.connect(/* ... */)
 //   .then(client => {
@@ -40,31 +35,6 @@ MongoClient.connect(connectionString, {
 //         console.log(result)
 //       })
 //       .catch(error => console.error(error))
-//   })
-
-
-
-
-// app.get('/', (req, res) => {
-//     res.send('Hello World')
-//     })
-
-// app.post('/quotes', (req, res) => {
-//     console.log('Hellooooooooooooooooo!')
-//   })
-
-
-// MongoClient.connect('mongodb-connection-string', (err, client) => {
-//     // ... do something here
-//     'mongodb+srv://Andrew:Chang@cluster0.jqbcy.mongodb.net/?retryWrites=true&w=majority'
-
-//     if (err) return console.error(err)
-//     console.log('Connected to Database')
-
-//   })
-
-// MongoClient.connect(connectionString, (err, client) => {
-//     'mongodb+srv://Andrew:Chang@cluster0.jqbcy.mongodb.net/?retryWrites=true&w=majority'
 //   })
 
 
