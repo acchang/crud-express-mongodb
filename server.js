d40cecb27d5521efde8729e9ff2b56a9868fc6d5
Andrew Chang <acchang4@gmail.com>
2022-06-02 19:03:29 -0400

    read is done

--- server.js
+++ server.js
@@ -7,25 +7,24 @@ const dotenv = require('dotenv') // .env file
 dotenv.config() // using .env
 const connectionString = `mongodb+srv://${process.env.USERNAME}:${process.env.PW}@cluster0.jqbcy.mongodb.net/?retryWrites=true&w=majority`;
 
-// All your handlers here...
-
-
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
   .then(client => {
     console.log('Connected to Database')
     const db = client.db('star-wars-quotes')
     const quotesCollection = db.collection('quotes')
+
+    app.set('view engine', 'ejs')
+
     app.use(bodyParser.urlencoded({ extended: true }))
 
     app.get('/', (req, res) => {
       db.collection('quotes').find().toArray()
       .then(results => {
-        console.log(results)
+        // console.log(results)
+        res.render('index.ejs', { quotes: results })
       })
       .catch(error => console.error(error))
-      
-      res.sendFile(`/Users/andrewchang/the_odin_project/crud-express-mongodb/index.html`)
-      })
+    })
       
     app.post('/quotes', (req, res) => {
       quotesCollection.insertOne(req.body)
