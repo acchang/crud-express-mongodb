832599473ad97c612c79ebdf9c0bc5694e59da04
Andrew Chang <acchang4@gmail.com>
2022-06-03 12:22:44 -0400

    all set

--- public/main.js
+++ public/main.js
@@ -1,93 +1,47 @@
 // main.js
+
 const update = document.querySelector('#update-button')
 const deleteButton = document.querySelector('#delete-button')
 const messageDiv = document.querySelector('#message')
 
 update.addEventListener('click', _ => {
-  fetch('/quotes', {
-    method: 'put',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      name: 'Darth Vadar',
-      quote: 'I find your lack of faith disturbing.'
+    fetch('/quotes', {
+      method: 'put',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        name: 'Darth Vadar',
+        quote: 'I find your lack of faith disturbing.'
+      })
     })
-  })
     .then(res => {
-      if (res.ok) return res.json()
-    })
-    .then(response => {
-      window.location.reload(true)
-    })
-})
+        if (res.ok) return res.json()
+      })
+      .then(response => {
+        window.location.reload(true)
+      })
+  })
 
 deleteButton.addEventListener('click', _ => {
-  fetch('/quotes', {
-    method: 'delete',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      name: 'Darth Vadar'
+    fetch('/quotes', {
+      method: 'delete',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        name: 'Darth Vadar'
+      })
     })
-  })
-    .then(res => {
-      if (res.ok) return res.json()
-    })
-    .then(response => {
-      if (response === 'No quote to delete') {
-        messageDiv.textContent = 'No Darth Vadar quote to delete'
-      } else {
-        window.location.reload(true)
-      }
-    })
-    .catch(console.error)
+      .then(res => {
+        if (res.ok) return res.json()
+      })
+    //   .then(data => {
+    //     window.location.reload()
+    //   })
+      .then(response => {
+        if (response === 'No quote to delete') {
+          messageDiv.textContent = 'No Darth Vadar quote to delete'
+        } else {
+          window.location.reload(true)
+        }
+      })
+      .catch(console.error)
+    //   .catch(error => console.error(error))
 })
-
-
-
-
-// // main.js
-
-// const update = document.querySelector('#update-button')
-// const deleteButton = document.querySelector('#delete-button')
-// const messageDiv = document.querySelector('#message')
-
-// update.addEventListener('click', _ => {
-//     fetch('/quotes', {
-//       method: 'put',
-//       headers: { 'Content-Type': 'application/json' },
-//       body: JSON.stringify({
-//         name: 'Darth Vadar',
-//         quote: 'I find your lack of faith disturbing.'
-//       })
-//     })
-//     .then(res => {
-//         if (res.ok) return res.json()
-//       })
-//       .then(response => {
-//         window.location.reload(true)
-//       })
-//   })
-
-// deleteButton.addEventListener('click', _ => {
-//     fetch('/quotes', {
-//       method: 'delete',
-//       headers: { 'Content-Type': 'application/json' },
-//       body: JSON.stringify({
-//         name: 'Darth Vadar'
-//       })
-//     })
-//       .then(res => {
-//         if (res.ok) return res.json()
-//       })
-//     //   .then(data => {
-//     //     window.location.reload()
-//     //   })
-//       .then(response => {
-//         if (response === 'No quote to delete') {
-//           messageDiv.textContent = 'No Darth Vadar quote to delete'
-//         } else {
-//           window.location.reload(true)
-//         }
-//       })
-//       .catch(console.error)
-//     //   .catch(error => console.error(error))
-// })
